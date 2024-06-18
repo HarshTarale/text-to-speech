add522263c56434f50ade09b5c79c2e99e9c2f5e
HarshTarale <taraleharsh0@gmail.com>
2024-06-18 15:24:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+window.addEventListener("load", () => {
+        const loadspeech = new SpeechSynthesisUtterance();
+        loadspeech.text ="Initializing System";
+        window.speechSynthesis.speak(loadspeech);
+      });
+      
+ const speech =()=>{
+        const text = document.getElementById("text").value;
+        if(text==""){     
+        const errorspeech = new SpeechSynthesisUtterance();
+        errorspeech.text ="no input..";
+        window.speechSynthesis.speak(errorspeech);
+
+        }
+        const convertspeech = new SpeechSynthesisUtterance();
+        convertspeech.text=text;
+        window.speechSynthesis.speak(convertspeech);
+ }
