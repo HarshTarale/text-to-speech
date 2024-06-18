c3304056f03d0298cca7d06e5fb6846076f9da43
HarshTarale <taraleharsh0@gmail.com>
2024-06-19 00:05:06 +0530

    Inisializing pop up add

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
+const  myDialog = document.getElementById("myDialog");
+const dialogCloseBtn =document.getElementById("dialogClose")
 
 window.addEventListener("load", () => {
-        const loadspeech = new SpeechSynthesisUtterance();
-        loadspeech.text ="Initializing System";
-        window.speechSynthesis.speak(loadspeech);
-      });
-      
+       dialogOpen();
+
+setTimeout(()=>{
+       console.log("load");
+       const errorspeech = new SpeechSynthesisUtterance();
+       errorspeech.text ="Initialising System ..";
+       window.speechSynthesis.speak(errorspeech);    
+},1000);
+
+});
+
+const dialogOpen =()=>{
+       myDialog.showModal();
+}
+
+dialogCloseBtn.addEventListener("click",()=>{
+       myDialog.close();
+})
+
  const speech =()=>{
-        const text = document.getElementById("text").value;
+        const text = document.getElementById("text").value.trim();
         if(text==""){     
         const errorspeech = new SpeechSynthesisUtterance();
         errorspeech.text ="no input..";
         window.speechSynthesis.speak(errorspeech);
-        
-
         }
         const convertspeech = new SpeechSynthesisUtterance();
         convertspeech.text=text;
