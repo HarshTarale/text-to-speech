ca75aa7b23de318c71249ab369c7c4cf28a64697
HarshTarale <taraleharsh0@gmail.com>
2024-06-19 21:08:38 +0530

    adding recoder

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 
 const  myDialog = document.getElementById("myDialog");
 const dialogCloseBtn =document.getElementById("dialogClose")
+
+
+
 window.addEventListener("load", () => {
        dialogOpen();
+       setTimeout(()=>{speaker("Initialising System ..")},1000);
 
-setTimeout(()=>{
-       console.log("load");
-       const errorspeech = new SpeechSynthesisUtterance(); 
-       errorspeech.text ="Initialising System ..";
-       window.speechSynthesis.speak(errorspeech);    
-},1000);
 });
+
+const speaker=(text)=>{
+       const errorspeech = new SpeechSynthesisUtterance(text); 
+       window.speechSynthesis.speak(errorspeech);  
+};
 
 const dialogOpen =()=>{
        myDialog.showModal();
 }
 
-dialogCloseBtn.addEventListener("click",()=>{
-       myDialog.close();
-})
-
  const speech =()=>{
-        const text = document.getElementById("text").value.trim();
-        if(text==""){     
-        const errorspeech = new SpeechSynthesisUtterance();
-        errorspeech.text ="no input..";
-        window.speechSynthesis.speak(errorspeech);
+       const  textin = document.getElementById("text").value.trim();
+
+        if(textin==""){     
+       
+       speaker("no input..")
+        } else {
+              speaker(textin);
         }
-        const convertspeech = new SpeechSynthesisUtterance();
-        convertspeech.text=text;
-        window.speechSynthesis.speak(convertspeech);
  }
+
+const recoder =()=>{
+
+       var recoding = new webkitSpeechRecognition();
+       recoding.lang = "en-US";
+       recoding.onresult= (event)=>{
+              const valuo = event.results[0][0].transcript;
+              document.getElementById("text").value=valuo;
+              speaker(valuo)
+       }
+       
+       recoding.start();
+       console.log("ok")  
+
+}
+
